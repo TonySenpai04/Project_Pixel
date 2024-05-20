31734d7836b4bb56d66723e91689d7e684f85a0d
TonySenpai04 <duongvansu006@gmail.com>
2024-05-20 22:14:21 +0700

    Refactor character data

---
+++ Assets/Tony/Script/Character/Abstract/CharacterData.cs
@@ -0,0 +1,24 @@
+
+using UnityEngine;
+namespace Tony
+{
+
+    public abstract class CharacterData : MonoBehaviour
+    {
+        public string characterID;
+        public string characterName;
+        public TextAsset csvFile;
+        public virtual void Start()
+        {
+
+        }
+        public virtual void SpeacialAbility()
+        {
+
+        }
+        public virtual void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Tony/Script/Character/Class/MeoChi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Tony
+{
+    public class MeoChi : CharacterData
+    {
+        
+        public override void Start()
+        {
+            SpeacialAbility();
+        }
+
+
+        public override void Update()
+        {
+
+        }
+        public override void SpeacialAbility()
+        {
+            Debug.Log("Tăng 0.1% nhận được trứng hiếm và tăng 10% vàng");
+        }
+    }
+}
